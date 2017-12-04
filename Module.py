3d178806f73efd76f949407df748b287e6a689e3
Eloy Parra Barrero <eloy.parrabarrero@ruhr-uni-bochum.de>
2017-12-04 11:29:34 +0100

    starting

---
+++ Module.py
@@ -0,0 +1,54 @@
+import numpy as np
+
+
+class Module:
+    def __init__(self, size, tau):
+        self.size = size
+        self.tau_head = tau
+        self.tau_errors = tau / 20
+
+        self.head = np.zeros(size)
+        self.head_out = np.zeros(size)
+        self.positive = np.zeros((2, size))
+        self.positive_out = np.zeros((2, size))
+        self.negative_out = np.zeros((2, size))
+
+        self.novelty = 0
+        self.inhibition = 0
+
+        self.child = None
+        self.parent = None
+
+        self.saturation = np.vectorize(self.saturation, otypes=[np.float])
+        self.activation = np.vectorize(self.activation, otypes=[np.float])
+
+    def is_child_of(self, parent):
+        assert isinstance(parent, Module)
+        self.parent = parent
+        parent.child = self
+
+    def step(self, predictions):
+        self.head += (-self.head + self.head_out + self.negative_out[0] - self.positive_out[0]) / self.tau_head
+        self.head_out = self.activation(self.head)
+        self.positive += (-self.positive + self.head_out - predictions) / self.tau_errors
+        self.positive_out = self.saturation(self.positive)
+        self.negative_out = self.saturation(-self.positive)
+        self.novelty = np.sum(self.positive_out[1])
+
+    def saturation(self, value):
+        """Saturates 'value' between 0 and 1"""
+        if value < 0:
+            return 0
+        elif value > 1:
+            return 1
+        else:
+            return value
+
+    def activation(self, value):
+        """Activation function for the head neurons"""
+        if value <= 0.25:
+            return 0
+        elif value <= 0.75:
+            return 2 * value - 0.5
+        else:
+            return 1
---
+++ test.py
@@ -0,0 +1,36 @@
+from Surganizing.Module import Module
+import numpy as np
+from matplotlib import pyplot as plt
+
+size = 3
+steps = 1000
+module = Module(size, 50)
+head_out = np.zeros((steps, size))
+novelty = np.zeros((steps, size))
+inhibition = np.zeros((steps, size))
+positive_out = np.zeros((steps, 2, size))
+negative_out = np.zeros((steps, 2, size))
+
+predictions = np.array([[1,0,0],[0,0,0]])
+
+for t in range(int(steps/2)):
+    module.step(predictions)
+    head_out[t] = module.head_out
+    positive_out[t] = module.positive_out
+    negative_out[t] = module.negative_out
+    novelty[t] = module.novelty
+
+predictions = np.array([[0,0,0],[0,0,0]])
+
+for t in range(int(steps/2), steps):
+    module.step(predictions)
+    head_out[t] = module.head_out
+    positive_out[t] = module.positive_out
+    negative_out[t] = module.negative_out
+    novelty[t] = module.novelty
+
+plt.plot(head_out[:,0])
+plt.plot(positive_out[:,0,0])
+plt.plot(negative_out[:,0,0])
+plt.plot(novelty[:,0])
+plt.show()
